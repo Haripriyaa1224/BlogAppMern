1c953593c081f0f291fd4a0dd7af38505ed12d94
Haripriyaa <mailtohari1224@gmail.com>
2024-07-25 16:52:17 +0530

    Routing Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
-    <div className='underline'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/sign-up' element={<SignUp/>}></Route>
+      <Route path='/sign-in' element={<SignIn/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/dashboard' element={<Dashboard />}></Route>
+      <Route path='/projects' element={<Projects />}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
