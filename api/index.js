8775d53a5ee5560a347ef2571063cead6a3351e1
Haripriyaa <mailtohari1224@gmail.com>
2024-07-29 15:53:41 +0530

    Connected to Database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv/config';
 
 const app = express();
+
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>{
+    console.log('connected successfully')
+})
+.catch(err =>
+    console.log(err)
+)
 
 app.listen(10000, ()=>{
     console.log('Server listening on port 10000')
