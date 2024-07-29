e777a8f84791263de5a5e759e0496d17a4601e98
Haripriyaa <mailtohari1224@gmail.com>
2024-07-29 16:02:22 +0530

    User Model Created

---
+++ api/models/user.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+//creating scehema
+const userSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type:String,
+        required: true
+    },
+}, {timestamps:true}
+);
+
+//creating model
+const User = mongoose.model('User', userSchema);
+
+export default User;
