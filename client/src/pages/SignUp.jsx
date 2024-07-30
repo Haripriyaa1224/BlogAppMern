3bfff1f335ac2a51c11192abc1829dd4b78da6d7
Haripriyaa <mailtohari1224@gmail.com>
2024-07-30 22:36:01 +0530

    Signup functionality added

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import {Button, Label, TextInput} from 'flowbite-react'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom';
+import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react'
+import axios from 'axios';
 
 const SignUp = () => {
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  
+
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.target.id]:e.target.value.trim()});
+    // console.log(formData)
+  }
+
+  const onSubmitHandler = async (e)=>{
+    e.preventDefault();
+
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+
+      
+      const res = await axios.post('/api/auth/signup', formData)
+      // console.log(res.data);
+      setLoading(false);
+      if(res.status === 200){
+        navigate('/sign-in');
+        
+      }
+    }catch(err){
+      // console.log(err.response.data.message);
+      setErrorMessage(err.response.data.message);
+      
+      setLoading(false);
+    }
+    
+    
+  }
+  
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -22,21 +61,23 @@ const SignUp = () => {
         {/* right */}
 
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={onSubmitHandler}>
             <div>
               <Label value='Your username' />
-              <TextInput type='text' placeholder='Username' id='username' />
+              <TextInput type='text' placeholder='Username' id='username' onChange={handleChange} required/>
             </div>
             <div>
               <Label value='Your email' />
-              <TextInput type='text' placeholder='name@company.com' id='email' />
+              <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange} required/>
             </div>
             <div>
               <Label value='Your password' />
-              <TextInput type='text' placeholder='Password' id='password' />
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange} required autoComplete="current-password"/>
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Sign Up
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                loading ? (<><Spinner size='sm' /><span className='pl-3'>Loading...</span></>) : 'Sign Up'
+              }
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -45,8 +86,10 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
+          {errorMessage && (<Alert className='mt-5' color='failure'>{errorMessage}</Alert>)}
         </div>
       </div>
+      
     </div>
  
   )
